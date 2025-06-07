7bb7dfec07260330af4d4257223c9fdabf6bc4c3
李勇 <1834211049@qq.com>
2025-06-07 16:27:11 +0800

    feat(services): 根据请求前缀配置IP

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,23 @@
 import axios from 'axios'
 
+
+const urlMap = new Map([
+	['/api', 'http://192.168.9.70:18089'],
+	['/ocrApi', 'http://192.168.0.130:5114'],
+]);
 // 请求拦截器
 axios.interceptors.request.use(config => {
 	// 添加请求头
 	config.headers.Authorization = localStorage.getItem('token') || ''
+	if (process.env.NODE_ENV === 'production') {
+		const { url } = config;
+
+		urlMap.forEach((ip, prefix) => {
+			if (url?.startsWith(prefix)) {
+				config.url = url.replace(prefix, ip);
+			}
+		});
+	}
 	return config
 }, error => {
 	return Promise.reject(error)
